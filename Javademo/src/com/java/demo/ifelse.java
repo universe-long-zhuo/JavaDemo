4a161de61e8d09235185b36ebac1f5a3d4dcd998
longzhuo <1009433228@qq.com>
2016-07-05 15:56:36 +0800

    0.1
    分支结构

---
+++ Javademo/src/com/java/demo/ifelse.java
@@ -0,0 +1,19 @@
+package com.java.demo;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		int x = 30;   // 数据类型  变量  赋值 30；
+
+	    if( x < 20 )  // if(布尔表达式)
+	        System.out.print("这是 if 语句");      //如果布尔表达式的值为true
+	    //else
+	        System.out.print("这是 else 语句 \n");  //如果布尔表达式的值为false
+	    
+	    System.out.println("wwwwwwwwww");
+	    
+
+	}
+
+}
