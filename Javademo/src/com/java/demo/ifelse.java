e1b2f4857a7b41235635e409be56e5fa79114bfa
longzhuo <1009433228@qq.com>
2016-11-14 15:16:12 +0800

    0.3

--- Javademo/src/com/java/demo/ifelse.java
+++ Javademo/src/com/java/demo/ifelse.java
@@ -4,12 +4,12 @@ public class ifelse {
 
 	public static void main(String[] args) {
 		
-		int x = 30;   // 数据类型 变量 赋值 30
+		int x = 30;   // 数据类型 变量 赋值 30 (初始化声明语句)
 
 	    if( x < 20 )  // if(布尔表达式)
 	        System.out.print("这是 if 语句");      //如果布尔表达式的值为true
 	    //else
-	        System.out.print("这是 else 语句 \n");  //如果布尔表达式的值为false
+	    System.out.print("这不是 else 语句 \n");  //如果布尔表达式的值为false
 	    
 	    System.out.println("wwwwwwwwww");
 	    
