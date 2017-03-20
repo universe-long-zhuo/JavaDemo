ebf2ce643a2799a841f9d18535969479422bfa5e
longzhuo <1009433228@qq.com>
2017-03-20 15:05:24 +0800

    0.6
    Java条件运算符 表达式1 < 表达式2 ？ 值 : 值；

---
+++ Javademo/src/com/java/demo2/main.java
@@ -0,0 +1,23 @@
+package com.java.demo2;    
+/**  
+ * @Title main.java  
+ * @description TODO  
+ * @time 2017年3月13日 下午3:38:42  
+ * @author wyz  
+ * @version 1.0  
+ **/
+public class main {
+
+	public static void main(String[] args) {
+		
+		int page;
+		page = -10;
+		
+		page = page < 1 ? 1 : page;
+		
+		System.out.println("page: " +page);
+
+	}
+
+}
+ 
